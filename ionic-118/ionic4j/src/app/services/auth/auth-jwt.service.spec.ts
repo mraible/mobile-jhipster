46958956e70df1acd6fd1699cfe9829b1ce0205c
Matt Raible <matt.raible@okta.com>
2019-07-24 00:09:11 -0400

    Initial application generated by JHipster-6.1.2

---
+++ ionic-118/ionic4j/src/app/services/auth/auth-jwt.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { AuthServerProvider } from './auth-jwt.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+
+describe('AuthServerProvider', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule, NgxWebstorageModule.forRoot()]
+  }));
+
+  it('should be created', () => {
+    const service: AuthServerProvider = TestBed.get(AuthServerProvider);
+    expect(service).toBeTruthy();
+  });
+});
---
+++ ionic-118/ionic4j/src/app/services/login/login.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+import { LoginService } from './login.service';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule } from '@ngx-translate/core';
+
+describe('LoginService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule, NgxWebstorageModule.forRoot(), TranslateModule.forRoot()]
+  }));
+
+  it('should be created', () => {
+    const service: LoginService = TestBed.get(LoginService);
+    expect(service).toBeTruthy();
+  });
+});
