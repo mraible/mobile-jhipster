d88b1ce49fb683bfe7d92414bee38b0aef9514c6
Matt Raible <matt.raible@okta.com>
2020-04-06 22:35:57 -0600

    Fix import

---
+++ ionic4j/src/app/auth/auth.module.ts
@@ -0,0 +1,31 @@
+import { Requestor, StorageBackend } from '@openid/appauth';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Platform } from '@ionic/angular';
+
+import { HttpClient } from '@angular/common/http';
+import { browserFactory, httpFactory, storageFactory } from './factories';
+import { Browser } from 'ionic-appauth';
+
+@NgModule({
+  imports: [CommonModule],
+  providers: [
+    {
+      provide: StorageBackend,
+      useFactory: storageFactory,
+      deps: [Platform],
+    },
+    {
+      provide: Requestor,
+      useFactory: httpFactory,
+      deps: [Platform, HttpClient],
+    },
+    {
+      provide: Browser,
+      useFactory: browserFactory,
+      deps: [Platform],
+    },
+  ],
+})
+export class AuthModule {}
