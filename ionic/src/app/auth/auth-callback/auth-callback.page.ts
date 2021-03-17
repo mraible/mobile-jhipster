73ce7e12c4b60457c167f1b700dfd60a792560df
Matt Raible <matt.raible@okta.com>
2021-03-17 11:31:40 -0600

    Generate Ionic App

---
+++ ionic/src/app/auth/auth-callback/auth-callback.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthActions, IAuthAction, AuthObserver, AuthService } from 'ionic-appauth';
+
+@Component({
+  template: '<p style="margin-left: 10px">Authorizing...</p>',
+})
+export class AuthCallbackPage implements OnInit, OnDestroy {
+  observer: AuthObserver;
+
+  constructor(private auth: AuthService, private navCtrl: NavController, private router: Router) {}
+
+  ngOnInit() {
+    this.observer = this.auth.addActionListener((action) => this.postCallback(action));
+    this.auth.authorizationCallback(window.location.origin + this.router.url);
+  }
+
+  ngOnDestroy() {
+    this.auth.removeActionObserver(this.observer);
+  }
+
+  postCallback(action: IAuthAction) {
+    if (action.action === AuthActions.SignInSuccess) {
+      this.navCtrl.navigateRoot('tabs');
+    }
+
+    if (action.action === AuthActions.SignInFailed) {
+      this.navCtrl.navigateRoot('login');
+    }
+  }
+}
