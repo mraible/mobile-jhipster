f1fa531921a077413460483b98f518958d5beaa8
Matt Raible <matt.raible@okta.com>
2021-11-10 00:38:39 -0500

    Use Keycloak by default

--- react-native/app/config/app-config.js
+++ react-native/app/config/app-config.js
@@ -8,7 +8,7 @@ export default {
   apiUrl: 'http://localhost:8080/',
   // leave blank if using Keycloak
   // - Dz7Oc9Zv9onjUBsdC55wReC4ifGMlA7G for Auth0
-  oktaClientId: '',
+  oktaClientId: '0oa2ki7yq5UuNf1P05d7',
   // use fixtures instead of real API requests
   useFixtures: false,
   // debug mode
